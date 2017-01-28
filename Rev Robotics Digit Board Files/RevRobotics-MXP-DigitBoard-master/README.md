be2ad2be4d098edbd95608cf8ea2aa6483007226
Randy <randy.default@gmail.com>
2017-01-27 21:21:22 -0700

    Added DigitBoard code, as well as starting points for Shooter code - Randy/Willow

---
+++ Rev Robotics Digit Board Files/RevRobotics-MXP-DigitBoard-master/README.md
@@ -0,0 +1,1 @@
+# RevRobotics-MXP-DigitBoard
---
+++ src/org/usfirst/frc/team498/robot/PewPew.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team498.robot;
+
+import edu.wpi.first.wpilibj.Talon;
+
+public class PewPew {
+
+	Talon talon;
+	
+	public PewPew() {
+		
+		
+		
+		
+	}
+	
+}
